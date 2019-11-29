8b680e8891c53915ea40ffe3eb011e75f7d88d02
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2019-11-28 21:04:09 -0800

    template for discrete exp families

--- src/ExponentialFamilies.jl
+++ src/ExponentialFamilies.jl
@@ -1,5 +1,31 @@
 module ExponentialFamilies
 
-greet() = print("Hello World!")
+using Reexport
+
+@reexport using Distributions
+@reexport using StatsBase
+
+using LinearAlgebra
+
+using Random
+import Random:rand, AbstractRNG
+
+import Statistics:mean, var
+
+import Distributions:probs, pdf, cf, mgf, support, sampler
+using StatsBase
+import StatsBase:moment
+
+using StatsModels
+
+import Base:eltype
+import Base.Broadcast: broadcastable
+
+
+include("discrete_exp_families.jl")
+
+export DiscreteExponentialFamilyModel,
+       DiscreteExponentialFamily
+
 
 end # module
